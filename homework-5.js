34a26cf85c9fb9f48f1597ae7623289f58105c54
Sanzhar Irgebay <theoneeetheonlyyy@gmail.com>
2025-11-01 18:25:29 +0500

    fix-function-name

--- homework-5.js
+++ homework-5.js
@@ -1,50 +1,50 @@
 //3 задание
-function findWeather (city, temperature){
-  console.log(`Сейчас в ${city} температура - ${temperature} градусов по Цельсию`)
+function determineWeather(city, temperature) {
+  console.log(`Сейчас в ${city} температура - ${temperature} градусов по Цельсию`);
 }
-findWeather('Осаке', 21)
+determineWeather('Осаке', 21);
 
 
 //4 задание
-const LIGHT_SPEED = 343
-function soundLevel(speed) {
-  if (speed > LIGHT_SPEED) {
-    console.log("Сверхзвуковая скорость")
+const LIGHT_SPEED = 343;
+function recognizeSpeed(speed) {
+  if(speed > LIGHT_SPEED) {
+    console.log("Сверхзвуковая скорость");
   }
-  else{
-    console.log("Дозвуковая скорость")
+  else {
+    console.log("Дозвуковая скорость");
   }
 }
-soundLevel(342)
+recognizeSpeed(342);
 
 
 //5 задание
-function trading(cur_budget) {
-let product = "яблоко"
+function buyProduct(budget) {
+let product = "яблоко";
 let price = 50;
-  if(cur_budget > price) {
-    console.log(`${product}, приобретёно. Спасибо за покупку!`)
+  if(budget > price) {
+    console.log(`${product}, приобретёно. Спасибо за покупку!`);
   }
-  else{
-    let dif_price = price - cur_budget
-    console.log(`Вам не хватает ${dif_price}, пополните баланс`)
+  else {
+    let diffCost = price - budget;
+    console.log(`Вам не хватает ${diffCost}, пополните баланс`);
   }
 }
-trading(51)
+buyProduct(51);
 
 
 // 6 и 7 Для улучшения аналитических способностей:
-let benchPress
-let squatting
-let deadlift
+let benchPress = 100;
+let squatting = 150;
+let deadlift = 170;
 
-function triathlon(benchPress,squatting,deadlift) {
-  let indicators = benchPress + squatting + deadlift
-  if (indicators>=250) {
-    console.log("Ваш результат хороший, если вы весите меньше 80 kg")
+function checkStrength(benchPress, squatting, deadlift) {
+  const INDICATORS = benchPress + squatting + deadlift;
+  if (INDICATORS >= 250) {
+    console.log("Ваш результат хороший, если вы весите меньше 80 kg");
   }
-  else{
-    console.log("Нужно постраться!")
+  else {
+    console.log("Нужно постраться!");
   }
 }
-triathlon(100, 100, 100)
+checkStrength(benchPress,squatting,deadlift);
