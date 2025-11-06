d546d8d722ef30c76f49653c11d9a84f3b98af05
Sanzhar Irgebay <theoneeetheonlyyy@gmail.com>
2025-11-06 23:54:02 +0500

    working-with-objects-and-arrays

---
+++ homework-6.js
@@ -0,0 +1,78 @@
+//3 task
+const personCartography = {
+  name : 'Sanzhar',
+  surname : 'Irgebay',
+  email : 'theoneeetheonlyyy@gmail.com',
+  work : 'programmer',
+  position : 'frontend developer',
+  age : 21,
+  country : 'Kazakhstan',
+  city : 'Astana',
+  relationship : 'single',
+  hobby : 'gym'
+}
+console.log(personCartography);
+
+//4 task
+const carSpecification = {
+  brand : 'Mercedes',
+  model : 'cls 400',
+  release : 2014,
+  color : 'black',
+  transmission : 'AT',
+}
+carSpecification.owner = personCartography.name;
+console.log(carSpecification);
+
+//5 task
+const getProperties = (carSpecification) => {
+  if (carSpecification.maximumSpeed === undefined) {
+    carSpecification.maximumSpeed = 250;
+  }
+  else {
+    return;
+  }
+}
+getProperties(carSpecification);
+
+//6 task
+const bringOut = (carSpecification, brand) => {
+  console.log(carSpecification[brand]);
+}
+bringOut(carSpecification,"brand");
+
+//7 task
+const groceryCards = ['mousse', 'mask', 'faceWashGel', 'giftSet-1', 'giftSet-5'];
+
+//8 task
+const greatBook = [
+  {name : "1984", autor: "George Orwell", year : 1949, coverColor : "gray", genre : "dystopian fiction",},
+  {name : "To Kill a Mockingbird", autor: "Harper Lee", year : 1960, coverColor : "blue", genre : "classic literature",},
+  {name : "The Hobbit", autor: "J.R.R. Tolkien", year : 1937, coverColor : "green", genre : "fantasy",}
+]
+greatBook.push({name : "The Da Vinci Code", autor: "Dan Brown", year : 2003, coverColor : "red", genre : "mystery thriller",});
+console.log(greatBook);
+
+//9 task
+const addingBook = [
+  {name : "1984", autor: "George Orwell", year : 1949, coverColor : "gray", genre : "dystopian fiction", universe: "Orwell Universe"},
+  {name : "To Kill a Mockingbird", autor: "Harper Lee", year : 1960, coverColor : "blue", genre : "classic literature", universe: "Southern Gothic"},
+  {name : "The Hobbit", autor: "J.R.R. Tolkien", year : 1937, coverColor : "green", genre : "fantasy", universe: "Middle-earth"}
+]
+greatBook.push({name : "The Da Vinci Code", autor: "Dan Brown", year : 2003, coverColor : "red", genre : "mystery thriller", universe: "Robert Langdon Universe"});
+
+const combinedBooks = [...greatBook, ...addingBook];
+console.log(combinedBooks);
+
+//10 task
+const checkRarity = (greatBook) => {
+  if(greatBook.year < 2000) {
+    greatBook.isRare = 'да, это редкий';
+  }
+  else {
+    greatBook.isRare = 'это не редкий';
+  }
+}
+
+greatBook.forEach(checkRarity);
+console.log(greatBook);
