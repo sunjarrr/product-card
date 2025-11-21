8fe1665cc53265e8749a5d1fc47a2dfffc88422d
Sanzhar Irgebay <theoneeetheonlyyy@gmail.com>
2025-11-21 20:54:20 +0500

    fix-function-tabulation-and-logic

--- homework-8.js
+++ homework-8.js
@@ -1,36 +1,40 @@
 //3. По аналогии из лекции — создать и реализовать шаблон для продуктовых карточек.
-import { cardContainers } from "./object.js";
+import { cards } from "./object.js";
 
 const cardTemplate = document.querySelector('.card-template');
-const cardList = document.querySelector('.all-card-container');
+const cardList = document.querySelector('.product-card-list'); 
 
-cardContainers.forEach(card => {
+function renderCards(cards) {
+cards.forEach(card => {
   const containerClone = cardTemplate.content.cloneNode(true);
   containerClone.querySelector('.product-category').textContent = card.type;
   containerClone.querySelector('.product-title').textContent = card.name;
   containerClone.querySelector('.product-description').textContent = card.description;
-  const ul = containerClone.querySelector('.product-composition');
-  card.structure.forEach(item => {
+  containerClone.querySelector('.product-price-label').textContent = card.priceLabel;
+  card.compound.forEach(item => {
+    const ul = containerClone.querySelector('.product-composition');
     const li = document.createElement('li');
     li.classList.add('product-composition-item');
     li.textContent = item;
     ul.appendChild(li);
   });
-  containerClone.querySelector('.card-image').src = card.img;
-  containerClone.querySelector('.card-container').classList.add(card.className);
+  containerClone.querySelector('.card-image').src = `${card.img}.png`;
   containerClone.querySelector('.product-price').textContent = card.price + ' \u20BD';
+  containerClone.querySelector('.card-container').classList.add('card-distance');
   cardList.appendChild(containerClone);
-})
+ })
+}
 
 
 //4. Используя метод .reduce(), получить строку, которая состоит из названий продуктовых карточек, разделенных точкой с запятой
-const getProductString = cardContainers.reduce((acc, products) => {
-  return acc += products.name + "; "
-},"")
+const getProductString = cards.reduce((acc, products) => {
+  acc.push(products.name);
+  return acc;
+}, []).join('; ');
 console.log(getProductString);
 
 //5. Используя метод .reduce(), получить массив объектов, где ключем является название продукта, а значением - его описание
-const getArrObjects = cardContainers.reduce((acc, value) => {
+const getArrObjects = cards.reduce((acc, value) => {
   acc[value.name] = value.description;
   return acc;
 }, {});
@@ -41,13 +45,13 @@ console.log(getArrObjects);
 Должна быть защита от введенных других значений (имеется ввиду проверка if).
 P.S - код из задания №3 переместить в функцию, не нужно его дублировать
 */
-function countCards (){
+function countCards() {
   const count = Number(prompt('Сколько карточек отобразить? От 1 до 5'))
     if (isNaN(count) || count < 1 || count > 5) {
       alert("Введите число от 1 до 5");
     }
     else {
-      console.log(cardContainers.slice(0, count));
+     renderCards(cards.slice(0, count));
     }
 }
 countCards();
